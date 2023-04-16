eb0f271c5c54d52bd34392534e8eb5547bbbf0fc
meilinru <77815163+meilinru@users.noreply.github.com>
2023-04-16 17:12:02 -0400

    trying another way

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-var http = require('http');
-var fs = require('fs');
-var minimist = require('minimist');
-const args = minimist(process.argv.slice(2));
-const port = args['port'] || 3000;
-var data_copy;
-fs.readFile('./public/index.html', 'utf8', (err, data) => {
+const http = require("http");
+const fs = require("fs");
+const minimist = require("minimist");
+const args = minimist(process.argv.slice());
+const port = args["port"] || 3000;
+
+fs.readFile('./public/index.html', (err, data) => {
 	if (err) {
 		console.error(err);
 		return;
 	}
-	data_copy= data;
-})	
-const server = http.createServer((req, res) => {	
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.end(data_copy);	
+	const server = http.createServer((_, res) => {	
+		res.statusCode = 200;
+		res.setHeader("Content-Type", 'text/html");
+		res.end(data);	
+	});
+	server.listen(port, () => {
+		console.log('Server listening on port ${port}');
+	});
 })
-server.listen(port);
-console.log('Server listening on port ${port}');
