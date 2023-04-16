a467a4c24654c5a52ae6aa8cf110eb9d39010cdd
meilinru <77815163+meilinru@users.noreply.github.com>
2023-04-16 16:38:20 -0400

    trying something new

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
-var http = require('http');
+const http = require('http');
 var fs = require('fs');
 var min = require('minimist')  
-const args = min(process.argv.slice(2));
-const port = args['port'] || 3000;
-var data_copy;
+let args = min(process.argv.slice(2), {
+		default: {
+port: 8080
+},
+});
+const port = args.port;
+
 fs.readFile('./public/index.html', 'utf8', (err, data) => {
 		if (err) {
 		console.error(err);
 		return;
 		}
-		data_copy= data;
-		})	
-const server = http.createServer((req, res) => {	
-		res.writeHeader(200, {'Content-Type': 'text/html'});
-		res.end(data_copy);	
-		})
-server.listen(port);
-console.log('Server listening on port ${port}');
+		console.log(data);	
+
+		const server = http.createServer((req, res) => {	
+				res.statusCode= 200;		
+				res.setHeader('Content-Type', 'text/html');
+				res.end(data);	
+				});
+		server.listen(port), () => {
+		console.log('Server listening on port ${port}');
+		});
+});
