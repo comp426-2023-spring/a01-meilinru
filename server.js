fd09e98a5db4de925f01ca0be377994f6615ea03
meilinru <77815163+meilinru@users.noreply.github.com>
2023-04-16 16:56:57 -0400

    adding semicolon pls

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 var http = require('http');
 var fs = require('fs');
-var min = require('minimist')  
+var min = require('minimist');
 const args = min(process.argv.slice(2));
 const port = args['port'] || 3000;
 var data_copy;
 fs.readFile('./public/index.html', 'utf8', (err, data) => {
-		if (err) {
+	if (err) {
 		console.error(err);
 		return;
-		}
-		data_copy= data;
-		})	
+	}
+	data_copy= data;
+})	
 const server = http.createServer((req, res) => {	
-		res.writeHeader(200, {'Content-Type': 'text/html'});
-		res.end(data_copy);	
-		})
+	res.writeHeader(200, {'Content-Type': 'text/html'});
+	res.end(data_copy);	
+})
 server.listen(port);
 console.log('Server listening on port ${port}');
