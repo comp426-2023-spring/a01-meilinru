4a180e807c515f73d8551aa63d8eff8e84dcb251
meilinru <77815163+meilinru@users.noreply.github.com>
2023-04-25 23:14:03 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
-var http = require('http');
-var fs = require('fs');
-var min = require('minimist')  
-const args = min(process.argv.slice(2));
-const port = args['port'] || 3000;
-data = fs.readFile('./public/index.html', 'utf8', (err, data) => {
-	if (err) {
-		console.error(err);
-		return;
-	}
-	console.log(data);
-});	
-const server = http.createServer((req, res) => {	
-   	res.statusCode = 200;
-   	res.setHeader('Content-Type', 'text/html');
-    	res.end(data);	
-});
-server.listen(port, () => {
-	console.log('Server listening on port ${port}');
-})
+const http = require("http");
+const fs = require("fs");
+const minimist = require("minimist");
+
+const args = minimist(process.argv.slice(2));
+const port = args.port || 3000;
+fs.readFile('./public/index.html','utf8', (err, data) => {
+    if (err) {
+      console.error();
+      return;
+    }
+    console.log(data);
+
+
+    const server = http.createServer((req, res) => {
+      // res.statusCode=200;
+      // res.setHeader('Content-Type', 'text/html');
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+
+  server.listen(port, () => {
+      console.log(`Server listening on port ${port}`);
+  });
+  });
